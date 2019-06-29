4b7ac42675e83ce26c20ee1307a6dd9f52ad90f0
StevenHo <steven.yyyz@live.cn>
2019-06-29 14:48:46 +0800

    networks and buffers

---
+++ buffer.py
@@ -0,0 +1,99 @@
+import torch
+import numpy as np
+import scipy.signal
+from utils.mpi_tools import mpi_statistics_scalar
+
+class Buffer(object):
+    def __init__(self, obs_dim, act_dim, batch_size, ep_len, dc_interv, gamma=0.99, lam=0.95, N=11):
+        self.max_batch = batch_size
+        self.dc_interv = dc_interv
+        self.max_s = batch_size * ep_len
+        self.obs_dim = obs_dim
+
+        self.obs = np.zeros((self.max_s, obs_dim))
+        self.act = np.zeros((self.max_s, act_dim))
+        self.rew = np.zeros(self.max_s)
+        self.end = np.zeros(batch_size + 1) # The first will always be 0
+
+        self.ptr = 0
+        self.eps = 0
+        self.dc_eps = 0
+
+        self.N = N
+        self.dc_buff = np.zeros((self.max_batch * self.dc_interv, self.N - 1, obs_dim))
+
+        self.gamma = gamma
+        self.lam = lam
+
+    def calc_diff(self):
+        # Store the differences of states into memory when epsiode ends
+        start = int(self.end[self.eps])
+        ep_l = self.ptr - start - 1
+        for i in range(self.N-1):
+            prev = int(i*ep_l/(self.N-1))
+            succ = int((i+1)*ep_l/(self.N-1))
+            self.dc_buff[self.eps, i] = self.obs[start + succ][:self.obs_dim] - self.obs[start + prev][:self.obs_dim]
+
+        return self.dc_buff[self.eps]
+
+    def retrieve_dc_buff(self):
+        assert self.dc_eps == self.max_batch * self.dc_interv
+        self.dc_eps = 0
+        return self.dc_buff
+
+class BufferS(Buffer):
+    def __init__(self, obs_dim, act_dim, batch_size, ep_len, dc_interv, gamma=0.99, lam=0.95, N=11):
+        super(BufferS, self).__init__(obs_dim, act_dim, batch_size, ep_len, dc_interv, gamma, lam, N)
+        self.ret = np.zeros(self.max_s)
+        self.adv = np.zeros(self.max_s)
+        self.pos = np.zeros(self.max_s) # The possibity that dc make it right
+        self.lgp = np.zeros(self.max_s)
+        self.val = np.zeros(self.max_s)
+        self.ent = np.zeros(self.max_s) # Entropy
+
+    def store(self, obs, act, rew, val, lgp):
+        assert self.ptr < self.max_s
+        self.obs[self.ptr] = obs
+        self.act[self.ptr] = act
+        self.rew[self.ptr] = rew
+        self.val[self.ptr] = val
+        self.lgp[self.ptr] = lgp
+        self.ptr += 1
+
+    def end_episode(self, pret_pos, last_val=0): # pret_pos gives the log possibility of cheating the discriminator
+        ep_slice = slice(int(self.end[self.eps]), self.ptr)
+        rewards = np.append(self.rew[ep_slice], last_val)
+        values = np.append(self.val[ep_slice], last_val)
+        deltas = rewards[:-1] + self.gamma * values[1:] - values[:-1]
+
+        returns = scipy.signal.lfilter([1], [1, float(-self.gamma)], rewards[::-1], axis=0)[::-1]
+        self.ret[ep_slice] = returns[:-1]
+        self.adv[ep_slice] = scipy.signal.lfilter([1], [1, float(-self.gamma * self.lam)], deltas[::-1], axis=0)[::-1]
+        self.pos[ep_slice] = pret_pos
+
+        self.eps += 1
+        self.dc_eps += 1
+        self.end[self.eps] = self.ptr
+
+    def retrieve_all(self):
+        assert self.eps == self.max_batch
+        occup_slice = slice(0, self.ptr)
+        self.ptr = 0
+        self.eps = 0
+        adv_mean, adv_std = mpi_statistics_scalar(self.adv[occup_slice])
+        pos_mean, pos_std = mpi_statistics_scalar(self.pos[occup_slice])
+        self.adv[occup_slice] = (self.adv[occup_slice] - adv_mean) / adv_std
+        self.pos[occup_slice] = (self.pos[occup_slice] - pos_mean) / pos_std
+        return [self.obs[occup_slice], self.act[occup_slice], self.adv[occup_slice], self.pos[occup_slice],
+            self.ret[occup_slice], self.lgp[occup_slice]]
+
+class BufferT(Buffer):
+    def __init__(self, obs_dim, act_dim, batch_size, ep_len, dc_interv, gamma=0.99, lam=0.95, N=11):
+        super(BufferT, self).__init__(obs_dim, act_dim, batch_size, ep_len, dc_interv, gamma, lam, N)
+
+    def store(self, obs, act, rew):
+        assert self.ptr < self.max_s
+        self.obs[self.ptr] = obs
+        self.act[self.ptr] = act
+        self.rew[self.ptr] = rew
+        self.ptr += 1
---
+++ network.py
@@ -0,0 +1,127 @@
+# Networks for value / policy / discriminator
+import numpy as np 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.distributions.categorical import Categorical
+from torch.distributions.normal import Normal
+from gym.spaces import Box, Discrete
+
+class MLP(nn.Module):
+    def __init__(self, layers, activation=torch.tanh, output_activation=None,
+                 output_squeeze=False):
+        super(MLP, self).__init__()
+        self.layers = nn.ModuleList()
+        self.activation = activation
+        self.output_activation = output_activation
+        self.output_squeeze = output_squeeze
+        
+        for i, layer in enumerate(layers[1:]):
+            self.layers.append(nn.Linear(layers[i], layer))
+            nn.init.zeros_(self.layers[i].bias)
+
+    def forward(self, input):
+        x = input
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        if self.output_activation is None:
+            x = self.layers[-1](x)
+        else:
+            x = self.output_activation(self.layers[-1](x))
+        return x.squeeze() if self.output_squeeze else x
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, input_dim, hidden_dims, activation, output_activation, action_dim):
+        super(GaussianPolicy, self).__init__()
+
+        self.mu = MLP(layers=[input_dim] + list(hidden_dims) + [action_dim], activation=activation, output_activation=output_activation)
+        self.log_std = nn.Parameter(-0.5 * torch.ones(action_dim))
+
+    def forward(self, x, a=None):
+        policy = Normal(self.mu(x), self.log_std.exp())
+        pi = policy.sample()
+        logp_pi = policy.log_prob(pi).sum(dim=1)
+        if a is not None:
+            logp = policy.log_prob(a).sum(dim=1)
+        else:
+            logp = None
+
+        return pi, logp, logp_pi
+
+class CategoricalPolicy(nn.Module):
+    def __init__(self, input_dim, hidden_dims, activation, output_activation, action_dim):
+        super(CategoricalPolicy, self).__init__()
+
+        self.logits = MLP(layers=[input_dim] + list(hidden_dims) + [action_dim], activation=activation)
+
+    def forward(self, x, a=None):
+        logits = self.logits(x)
+        policy = Categorical(logits=logits)
+        pi = policy.sample()
+        logp_pi = policy.log_prob(pi).squeeze()
+        if a is not None:
+            logp = policy.log_prob(a).squeeze()
+        else:
+            logp = None
+
+        return pi, logp, logp_pi
+
+class BLSTMPolicy(nn.Module):
+    def __init__(self, input_dim, hidden_dims, activation, output_activation, con_dim):
+        super(BLSTMPolicy, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dims//2, batch_first=True, bidirectional=True)
+        self.linear = nn.Linear(hidden_dims, con_dim)
+        nn.init.zeros_(self.linear.bias)
+
+    def forward(self, seq, gt=None):
+        inter_states, _ = self.lstm(seq)
+        logit_seq = self.linear(inter_states)
+        self.logits = torch.mean(logit_seq, dim=1)
+        policy = Categorical(logits=self.logits)
+        label = policy.sample()
+        logp = policy.log_prob(label).squeeze()
+        if gt is not None:
+            loggt = policy.log_prob(gt).squeeze()
+        else:
+            loggt = None
+
+        return label, loggt, logp
+
+class ActorCritic(nn.Module):
+    def __init__(self, input_dim, action_space, hidden_dims=(64, 64), activation=torch.tanh, output_activation=None, policy=None):
+        super(ActorCritic, self).__init__()
+
+        if policy is None:
+            if isinstance(action_space, Box):
+                self.policy = GaussianPolicy(input_dim, hidden_dims, activation, output_activation, action_space.shape[0])
+            elif isinstance(action_space, Discrete):
+                self.policy = CategoricalPolicy(input_dim, hidden_dims, activation, output_activation, action_space.n)
+        else: 
+            self.policy = policy(input_dim, hidden_dims, activation, output_activation, action_space)
+
+        self.value_f = MLP(layers=[input_dim] + list(hidden_dims) + [1], activation=activation, output_squeeze=True)
+
+    def forward(self, x, a=None):
+        pi, logp, logp_pi = self.policy(x, a)
+        v= self.value_f(x)
+
+        return pi, logp, logp_pi, v
+
+# Bidirectional LSTM for encoding trajectories
+# Batch-first used
+# input: (batch_size, seq_len, input_dim)
+# inter_state: (batch_size, seq_len, 2*hidden_dims)
+# linear_output: (batch_size, seq_len, context_dim)
+# avg_logits: (batch_size, context_dim)
+class Discriminator(nn.Module):
+    def __init__(self, input_dim, context_dim, activation=torch.softmax, output_activation=torch.softmax, hidden_dims=64):
+        super(Discriminator, self).__init__()
+
+        self.policy = BLSTMPolicy(input_dim, hidden_dims, activation, output_activation, context_dim)
+
+    def forward(self, seq, gt=None):
+        pred, loggt, logp = self.policy(seq, gt)
+        return pred, loggt, logp
+
+def count_vars(module):
+    return sum(p.numel() for p in module.parameters() if p.requires_grad)
